42d813f266e5bd1e433d38f6d86ecf050a9a9489
Sudipta <techtv70@gmail.com>
2025-09-22 14:01:35 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="w-full h-screen grid grid-cols-12 bg-[]"></div>;
+  return (
+    <div className="w-full font-[Outfit] h-screen bg-blue-600 bg-[url('src/assets/Hero-Background-notecode.svg')] bg-cover bg-center">
+      <div className="w-full flex justify-center items-center p-6">
+        <span>
+          <img src="src/assets/NoteCodeLogo.svg" width={170} />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;

a14049557a5732cdad81a1ccf9f822dc72e47f53
Sudipta <techtv70@gmail.com>
2025-09-22 17:14:08 +0530

    .

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full font-[Outfit] h-screen bg-blue-600 bg-[url('src/assets/Hero-Background-notecode.svg')] bg-cover bg-center">
+    <div className="w-full font-[Outfit] min-h-screen bg-blue-600 bg-[url('src/assets/Hero-Background-notecode.svg')] bg-cover bg-center">
       <div className="text-center p-6">
         <div className="mb-8 pr-4">
           <img
@@ -16,8 +16,8 @@ function App() {
           <p className="text-4xl ">Your Code Easily</p>
         </div>
       </div>
-      <div className="flex justify-center h-full">
-        <div className="h-full w-4xl bg-white rounded-lg "></div>
+      <div className="flex justify-center w-full h-[700px]  ">
+        <div className=" w-full mx-6 sm:w-4xl bg-white rounded-lg ">hi</div>
       </div>
     </div>
   );

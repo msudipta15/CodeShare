1cdcf299b6b70ec03a00b4d6c0240b6af1271740
Sudipta <techtv70@gmail.com>
2025-09-24 19:50:19 +0530

    .

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+async function ConnectDB() {
+  const db_url = process.env.DB_URL;
+  if (!db_url) {
+    console.log("Invalud DB_URL");
+    return;
+  }
+  console.log("Connecting...");
+  await mongoose.connect(db_url);
+  console.log("Connected to database");
+}
+
+ConnectDB();
 
 const app = express();
 
